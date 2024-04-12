fa64a52c83ae511d0c10d1176773a6f90f2f0185
Pravesh Bhandari <02230270.cst@rub.edu.bt>
2024-04-12 16:34:05 +0530

    changes to CAP_02230270.py

--- CAP_02230270.py
+++ CAP_02230270.py
@@ -17,7 +17,7 @@ def read_input():
     return open('CSF101CAP/input_0_cap1.txt','r')
 
 def calculate_score(file):
-    score_dict = {'A X': 1, 'A Y': 4, 'A Z': 7, 'B X': 2, 'B Y': 5, 'B Z': 8, 'C X': 3, 'C Y': 6, 'C Z': 9} 
+    score_dict = {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7} 
     total_value = 0
     for line in file:
         value = line.strip()
