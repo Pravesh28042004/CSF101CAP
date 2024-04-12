5d7ab8806791772587b56e4e32cd372c334cd2c9
Pravesh28042004 <159408266+Pravesh28042004@users.noreply.github.com>
2024-04-12 22:29:21 +0530

    Update CAP_02230270.py

--- CAP_02230270.py
+++ CAP_02230270.py
@@ -10,14 +10,14 @@
 # https://realpython.com/read-write-files-python/
 ################################
 # SOLUTION
-# Your Solution Score: 46548
+# Your Solution Score: 49894
 # Put your number here: 0
 ################################
 def read_input():
     return open('CSF101CAP/input_0_cap1.txt','r')
 
 def calculate_score(file):
-    score_dict = {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7} 
+    score_dict = {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
     total_value = 0
     for line in file:
         value = line.strip()
