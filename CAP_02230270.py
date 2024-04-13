65ab4ae427395cde364e326d0f30a9560bf503ec
Pravesh28042004 <159408266+Pravesh28042004@users.noreply.github.com>
2024-04-13 10:39:25 +0530

    Update CAP_02230270.py

--- CAP_02230270.py
+++ CAP_02230270.py
@@ -17,12 +17,12 @@ def read_input():
     return open('CSF101CAP/input_0_cap1.txt','r')
 
 def calculate_score(file):
-    score_dict = {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
+    point_dictionary = {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
     total_value = 0
-    for line in file:
-        value = line.strip()
-        if value in score_dict:
-            total_value += score_dict[value]
+    for row in file:
+        value = row.strip()
+        if value in point_dictionary:
+            total_value += point_dictionary[value]
     print("total value: ",total_value)
 
 calculate_score(read_input())
